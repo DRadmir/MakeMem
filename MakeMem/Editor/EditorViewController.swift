1db74f688ffebbe7d6e66bd6357e292cd8592127
DRadmir <Radmir@sibmail.com>
2020-01-17 19:18:02 +0700

    now i create textfield

--- MakeMem/Editor/EditorViewController.swift
+++ MakeMem/Editor/EditorViewController.swift
@@ -14,6 +14,7 @@ class EditorViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var addLabelButton: UIButton!
     @IBOutlet weak var imageButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
     
     var currentImage: UIImage?
     var image = [ItemsInfo]()
@@ -22,17 +23,33 @@ class EditorViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        saveButton.isUserInteractionEnabled = false
+        
         imageView.image = currentImage
         
     }
     
-    @IBAction func addLabelAction(_ sender: UIButton) {
+    @IBAction func addLabelButtonAction(_ sender: UIButton) {
     }
     
-    @IBAction func addImageButton(_ sender: UIButton) {
+    @IBAction func addImageButtonAction(_ sender: UIButton) {
+        
+        self.chooseImagePicker(source: .photoLibrary)
+        
     }
     
-
+    @IBAction func saveButtonAction(_ sender: UIButton) {
+        
+        if imageView.image != nil {
+            
+            self.saveButton.isUserInteractionEnabled = true
+            
+            guard let image = currentImage else { return }
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+            
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -43,4 +60,27 @@ class EditorViewController: UIViewController {
     }
     */
 
+}
+
+extension EditorViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        imageView.image = info[.editedImage] as? UIImage
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
 }
